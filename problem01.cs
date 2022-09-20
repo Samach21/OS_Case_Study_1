79535a11e0b726cd864420067cda84f68a4e1e77
samach21 <samach21@gmail.com>
2022-09-21 00:17:43 +0700

    v3

--- problem01.cs
+++ problem01.cs
@@ -13,7 +13,7 @@ namespace Problem01
         const int threadSize = 12;
         static byte[] Data_Global = new byte[dataSize];
         static long Sum_Global = 0;
-        static long[] Pre_Sum_Global = new long[12];
+        static long[] Pre_Sum_Global = new long[threadSize];
         static int G_index = 0;
 
         static int ReadData()
@@ -60,113 +60,25 @@ namespace Problem01
         }
         static void TestThread(int threadIndex)  
         {
-            int start = threadIndex * (dataSize/threadSize);
-            int stop = (threadIndex + 1) * (dataSize/threadSize);
+            double step = dataSize/threadSize;
+            int start = Convert.ToInt32(threadIndex * step);
+            int stop = Convert.ToInt32((threadIndex + 1.0d) * step);
+            // Console.WriteLine("{0} {1} {2}", threadIndex, start, stop);
             for (int i = start; i < stop; i++)
             {
                 sum(threadIndex, i);
-            }
-        }
-        static void Thread1()  
-        {
-            for (int i = 0; i < 83333333; i++)
-            {
-                sum(0, i);
-            }
-        }
-        static void Thread2()  
-        {
-            for (int i = 83333333; i < 166666666; i++)
-            {
-                sum(1, i);
-            }
-        }
-        static void Thread3()  
-        {
-            for (int i = 166666666; i < 250000000; i++)
-            {
-                sum(2, i);
-            }
-        }
-        static void Thread4()  
-        {
-            for (int i = 250000000; i < 333333333; i++)
-            {
-                sum(3, i);
-            }
-        }
-        static void Thread5()  
-        {
-            for (int i = 333333333; i < 416666666; i++)
-            {
-                sum(4, i);
-            }
-        }
-        static void Thread6()  
-        {
-            for (int i = 416666666; i < 500000000; i++)
-            {
-                sum(5,  i);
-            }
-        }
-        static void Thread7()  
-        {
-            for (int i = 500000000; i < 583333333; i++)
-            {
-                sum(6, i);
-            }
-        }
-        static void Thread8()  
-        {
-            for (int i = 583333333; i < 666666666; i++)
-            {
-                sum(7, i);
-            }
-        }
-        static void Thread9()  
-        {
-            for (int i = 666666666; i < 750000000; i++)
-            {
-                sum(8, i);
-            }
-        }
-        static void Thread10()  
-        {
-            for (int i = 750000000; i < 833333333; i++)
-            {
-                sum(9, i);
-            }
-        }
-        static void Thread11()  
-        {
-            for (int i = 833333333; i < 916666666; i++)
-            {
-                sum(10, i);
-            }
-        }
-        static void Thread12()  
-        {
-            for (int i = 916666666; i < 1000000000; i++)
-            {
-                sum(11, i);
             }
         }
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
             int y;
-            Thread th1 = new Thread(Thread1);
-            Thread th2 = new Thread(Thread2);
-            Thread th3 = new Thread(Thread3);
-            Thread th4 = new Thread(Thread4);
-            Thread th5 = new Thread(Thread5);
-            Thread th6 = new Thread(Thread6);
-            Thread th7 = new Thread(Thread7);
-            Thread th8 = new Thread(Thread8);
-            Thread th9 = new Thread(Thread9);
-            Thread th10 = new Thread(Thread10);
-            Thread th11 = new Thread(Thread11);
-            Thread th12 = new Thread(Thread12);
+            Thread[] th = new Thread[threadSize];
+            for (int i = 0; i < threadSize; i++)
+            {
+                int localNum = i;
+                th[i] = new Thread(()=>TestThread(localNum));
+            }
 
             for (int i = 0; i < threadSize; i++)
             {
@@ -186,32 +98,16 @@ namespace Problem01
             }
 
             /* Start */
-            Console.Write("\n\nWorking...");
+            Console.Write("\n\nWorking...\n");
             sw.Start();
-            th1.Start();
-            th2.Start();
-            th3.Start();
-            th4.Start();
-            th5.Start();
-            th6.Start();
-            th7.Start();
-            th8.Start();
-            th9.Start();
-            th10.Start();
-            th11.Start();
-            th12.Start();
-            th1.Join();
-            th2.Join();
-            th3.Join();
-            th4.Join();
-            th5.Join();
-            th6.Join();
-            th7.Join();
-            th8.Join();
-            th9.Join();
-            th10.Join();
-            th11.Join();
-            th12.Join();
+            for (int i = 0; i < threadSize; i++)
+            {
+                th[i].Start();
+            }
+            for (int i = 0; i < threadSize; i++)
+            {
+                th[i].Join();
+            }
             Sum_Global = Pre_Sum_Global.Sum();
             sw.Stop();
             Console.WriteLine("Done.");
